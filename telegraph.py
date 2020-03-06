cba9cb32a170f7da402794939ec850f0771bfb6f
PXRL54 <rom.terekh2017@ya.ru>
2020-03-06 16:59:35 +0300

    Update telegraph.py

--- telegraph.py
+++ telegraph.py
@@ -12,28 +12,39 @@ print(url)
 print('---------')
 
 def download():
+	kavoS = 0
 	page = requests.get(url)
+
 	print(page.status_code)
+
 	soup = bs(page.text, "html.parser")
 	t = soup.findAll('title')
 	t = str(t)
 	t = t[8:-9]
 	print(t)
+
 	images = soup.findAll('img')
+
 	for i in range(0,len(images)):
 		img = images[i]
 		img = str(img)
 		img = img[10:-3]
 		path.append(img)
 		print(path[i])
+
 	print("----------")
 	print(str(len(path)) + ' images found!')
+	
 	os.mkdir(t)
 	os.chdir(t)
 	for i in range(0,len(path)):
 		wget.download("https://telegra.ph" + str(path[i]), str(i) + ".png")
 		print(' ')
-		print(str(i) + '.png was succesfully downloaded!')
+		kavoS = kavoS + os.path.getsize(str(i) + '.png')
+		print(str(i) + '.png was succesfully downloaded! ' + str(i+1) + '/' + str(len(path)) + '  Curent size - ' + str(round(kavoS/1024/1024, 1)) + 'M')
+		
+	print('-----------')
+	print(str(round(kavoS/1024/1024, 1)) + ' M total ' + t)
 
 if 'telegra.ph' in url:
 	print("All done! It's a telegraph link")
